761bc037d31b75df69c0c03e974152593035dcd5
SilvaEmerson <emersonjose877@gmail.com>
2020-03-13 16:47:25 -0300

    Add front

---
+++ front/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+function App() {
+  const [conn] = useState(() => {
+    return io("https://test.api.pedeaivai.com.br", {
+        transports: ['websocket'],
+    });
+  });
+
+  const [messages, setMessages] = useState([]);
+  const [roomId, setRoomId] = useState();
+
+  useEffect(() => {
+    conn.on("solicitation", data => {
+      setMessages(currMessages => [...currMessages, data]);
+    });
+
+    conn.on("message-channel", (data, ackFn) => {
+      console.log(data);
+      ackFn && ackFn(true);
+      setMessages(currMessages => [...currMessages, data]);
+    });
+  }, [conn]);
+
+  useEffect(() => {
+    conn.emit("room", { roomId: roomId });
+  }, [roomId]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={ev => {
+          console.log(ev.target.value);
+          setRoomId(ev.target.value);
+        }}
+      ></input>
+      {messages.map((el, ind) => (
+        <ul key={ind}>{el.message}</ul>
+      ))}
+    </div>
+  );
+}
+
+export default App;
