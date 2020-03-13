5b7921695eb2487c709bc1f4fffbb61d8752bb91
SilvaEmerson <emersonjose877@gmail.com>
2020-03-13 14:04:25 -0300

    Add CORS

--- src/server.js
+++ src/server.js
@@ -1,35 +1,43 @@
 var app = require("express")();
 const express = require("express");
+const cors = require("cors");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", function(req, res) {
   res.send("Hello");
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body)
   io.sockets
     .in(req.body.roomId)
     .emit("solicitation", { message: req.body.message });
   res.send("New solicitation");
 });
 
-const nsp = io.of('/custom-namespace');
+app.post("/demo", function(req, res) {
+  io.emit("message-channel", { message: req.body.message });
+  res.send("New solicitation");
+});
 
-nsp.on('connection', (socket) => {
-  console.log("Someone connected to custom-namespace")
-  nsp.emit('message-channel', {message: "Hello"})
-})
+const nsp = io.of("/custom-namespace");
+
+nsp.on("connection", socket => {
+  console.log(`${socket.conn.remoteAddress} connected to custom-namespace`);
+  nsp.emit("message-channel", { message: "Hello" });
+});
 
 io.on("connection", function(socket) {
-  console.log("Someone connected");
-  io.emit('message-channel', {message: "Hello"})
-  socket.on("room", function(data) {
-    socket.join(data.roomId);
+  console.log(`${socket.conn.remoteAddress} connected`);
+  socket.emit("message-channel", { message: "Hello" }, () => {
+    console.log("Message received by client");
   });
+   socket.on("room", function(data) {
+     socket.join(data.roomId);
+   });
 });
 
 server.listen(3001);
